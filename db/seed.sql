9d2cdbe5cac41a387e973dbae7f01017d566efd1
David Turner <Dturner753@yahoo.com>
2018-03-26 11:45:52 -0700

    burger project with added protections

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgers_name) VALUES ('Cheese burger');
+INSERT INTO burgers (burgers_name) VALUES ('Double cheeseburger');
+INSERT INTO burgers (burgers_name) VALUES ('Plain burger');
